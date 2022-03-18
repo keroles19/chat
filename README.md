3f3aa4ff3158cdd9279cbc1acf906ebac649b522
keroles19 <keroleate200@gmail.com>
2022-03-18 22:30:26 +0200

    add readme file

---
+++ README.md
@@ -0,0 +1,37 @@
+
+## chat App Priview
+<video src='https://user-images.githubusercontent.com/36054945/159077940-700228b3-b932-4870-8d0c-7b13c6706557.mp4
+'width=180/>
+## KCaht App
+Chat app by : - Laravel - Mysql  - Vue Js - laravel echo - event and listner 
+
+
+## Instructions
+ Pre-steps
+ 
+  Make sure you apache server working and create an empty database and name it messages.
+  
+  Upload **(sql database\messages.sql )** file to database.
+
+
+## How to ?
+- Open apache and mysql server
+- copy .env.example file and rename it to .env 
+- change your sql env 
+- change pusher configuration 
+- php artisan key:generate
+- composer install 
+- npm install
+- run project 
+                                                                             
+
+## Built With
+- **lphp - phpMyAdmin - Xamp**
+- **laravel - vue js - vuetify package** 
+- **mySql**
+
+## simples
+chat App
+<p align="center"><img src="https://user-images.githubusercontent.com/36054945/159078709-6da32865-b502-46ba-b100-8dc49618a35a.png" width="400"></p>
+
+
---
+++ messages.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2022 at 09:05 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `user_id`, `message`, `created_at`, `updated_at`, `receiver_id`) VALUES
+(13, 9, 'welcom', '2022-03-18 17:44:38', '2022-03-18 17:44:38', 2),
+(14, 2, 'welcome sit', '2022-03-18 17:44:44', '2022-03-18 17:44:44', 9),
+(15, 9, 'kero2', '2022-03-18 17:45:01', '2022-03-18 17:45:01', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
